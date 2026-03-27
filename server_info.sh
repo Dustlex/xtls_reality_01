c60ab4c84e1de6317be5018c8d64ed30a2b5d3d2
Dustlex <140817344+Dustlex@users.noreply.github.com>
2026-03-27 20:29:05 +0300

    Update server_info.sh
    new extraction script for xhttp transport

--- server_info.sh
+++ server_info.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-echo "PublicKey(Pbk) = `docker compose exec xray bash -c "cat /home/PBK.txt"`"
-echo "PrivateKey(Prk) = `docker compose exec xray bash -c "sed -n -e 34p /opt/xray/config.json" | awk -F '"' '{print $4}'`"
-echo "UUID = `docker compose exec xray bash -c "sed -n -e 17p /opt/xray/config.json" | awk -F '"' '{print $4}'`"
-echo "Domain(SNI) = `docker compose exec xray bash -c "sed -n -e 32p /opt/xray/config.json" | awk -F '"' '{print $2}'`"
-echo "ShortID(Sid) = `docker compose exec xray bash -c "sed -n -e 39p /opt/xray/config.json" | awk -F '"' '{print $2}'`"
+echo "Domain(SNI) = `docker compose exec xray bash -c "sed -n -e 49p /opt/xray/config.json" | awk -F '"' '{print $2}'`"
+echo "UUID = `docker compose exec xray bash -c "sed -n -e 33p /opt/xray/config.json" | awk -F '"' '{print $4}'`"
+echo "ShortID(SID) = `docker compose exec xray bash -c "sed -n -e 57p /opt/xray/config.json" | awk -F '"' '{print $2}'`"
+echo "PrivateKey(PRK) = `docker compose exec xray bash -c "sed -n -e 52p /opt/xray/config.json" | awk -F '"' '{print $4}'`"
+echo "PublicKey(PBK) = `docker compose exec xray bash -c "cat /home/PBK.txt"`"
