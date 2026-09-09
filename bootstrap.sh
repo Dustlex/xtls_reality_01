475a274d2629656cfde51ebe2719526aaedc94b8
Dustlex <140817344+Dustlex@users.noreply.github.com>
2026-09-09 19:54:28 +0300

    New_xray_core_update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-K=${XRAY_K:-$(/opt/xray/xray x25519 | awk '{print $2}')}
+K=${XRAY_K:-$(/opt/xray/xray x25519 | awk -F ' ' '{print $NF}')}
 export PRK=${PRK:-$(echo "$K" | sed -n 1p)}
 export PBK=${PBK:-$(echo "$K" | sed -n 2p)}
 export UUID=${UUID:-$(/opt/xray/xray uuid)}
 export SID=${SID:-$(openssl rand -hex 8)}
+export XHTTP_PATH=${XHTTP_PATH:-$(openssl rand -hex 8)}
+
 echo "$PBK" > /home/PBK.txt
+echo "$XHTTP_PATH" > /home/XHTTP_PATH.txt
+
 envsubst < /opt/xray/config.json_prepare_2 > /opt/xray/config.json
 /opt/xray/xray run -c /opt/xray/config.json
