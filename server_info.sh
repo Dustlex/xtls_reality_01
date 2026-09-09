38f0e6bb8b4fc9a1daf9bda571a217ea35af9c61
Dustlex <140817344+Dustlex@users.noreply.github.com>
2026-09-09 19:58:44 +0300

    New_info_with_fast_cinnect_URL

--- server_info.sh
+++ server_info.sh
@@ -1,6 +1,23 @@
-#! /bin/bash
-echo "Domain(SNI) = `docker compose exec xray bash -c "sed -n -e 49p /opt/xray/config.json" | awk -F '"' '{print $2}'`"
-echo "UUID = `docker compose exec xray bash -c "sed -n -e 33p /opt/xray/config.json" | awk -F '"' '{print $4}'`"
-echo "ShortID(SID) = `docker compose exec xray bash -c "sed -n -e 57p /opt/xray/config.json" | awk -F '"' '{print $2}'`"
-echo "PrivateKey(PRK) = `docker compose exec xray bash -c "sed -n -e 52p /opt/xray/config.json" | awk -F '"' '{print $4}'`"
-echo "PublicKey(PBK) = `docker compose exec xray bash -c "cat /home/PBK.txt"`"
+#!/bin/bash
+set -euo pipefail
+
+CFG=$(docker compose exec -T xray cat /opt/xray/config.json)
+IP=$(curl -s -4 https://ip.sb || curl -s -4 https://ifconfig.me)
+
+DOM=$(echo "$CFG" | jq -r '.inbounds[0].streamSettings.realitySettings.serverNames[0]')
+UUID=$(echo "$CFG" | jq -r '.inbounds[0].settings.clients[0].id')
+SID=$(echo "$CFG" | jq -r '.inbounds[0].streamSettings.realitySettings.shortIds[0]')
+PRK=$(echo "$CFG" | jq -r '.inbounds[0].streamSettings.realitySettings.privateKey')
+#XPATH=$(echo "$CFG" | jq -r '.inbounds[1].streamSettings.xhttpSettings.path' | sed 's#^/##')
+PBK=$(docker compose exec -T xray cat /home/PBK.txt)
+
+echo "Domain(SNI) = $DOM"
+echo "UUID = $UUID"
+echo "ShortID(SID) = $SID"
+echo "PrivateKey(PRK) = $PRK"
+echo "PublicKey(PBK) = $PBK"
+#echo "XHTTP_PATH = $XPATH"
+echo ""
+echo "VLESS (TCP/Vision) = vless://${UUID}@${IP}:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${DOM}&fp=chrome&pbk=${PBK}&sid=${SID}&type=tcp&alpn=h2,http/1.1#${DOM}-Vision"
+echo "VLESS (XHTTP)      = vless://${UUID}@${IP}:8443?encryption=none&security=reality&sni=${DOM}&fp=chrome&pbk=${PBK}&sid=${SID}&type=xhttp&path=%2F&mode=auto&alpn=h2#${DOM}-XHTTP"
+#echo "VLESS (XHTTP)      = vless://${UUID}@${IP}:8443?encryption=none&security=reality&sni=${DOMAIN}&fp=chrome&pbk=${PBK}&sid=${SID}&type=xhttp&path=%2F${XPATH}&mode=auto&alpn=h2#${DOMAIN}-XHTTP"
