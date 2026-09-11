ad5d1b76173840bd608515b3518ff9d20284866b
Dustlex <140817344+Dustlex@users.noreply.github.com>
2026-09-11 19:27:29 +0300

    Refactor server_info.sh to improve config extraction

--- server_info.sh
+++ server_info.sh
@@ -3,21 +3,38 @@ set -euo pipefail
 
 CFG=$(docker compose exec -T xray cat /opt/xray/config.json)
 IP=$(curl -s -4 https://ip.sb || curl -s -4 https://ifconfig.me)
-
-DOM=$(echo "$CFG" | jq -r '.inbounds[0].streamSettings.realitySettings.serverNames[0]')
-UUID=$(echo "$CFG" | jq -r '.inbounds[0].settings.clients[0].id')
-SID=$(echo "$CFG" | jq -r '.inbounds[0].streamSettings.realitySettings.shortIds[0]')
-PRK=$(echo "$CFG" | jq -r '.inbounds[0].streamSettings.realitySettings.privateKey')
-#XPATH=$(echo "$CFG" | jq -r '.inbounds[1].streamSettings.xhttpSettings.path' | sed 's#^/##')
+DOM=$(echo "$CFG" | jq -r '.inbounds[] | select(.tag=="inbound-443") | .streamSettings.realitySettings.serverNames[0]')
+UUID=$(echo "$CFG" | jq -r '.inbounds[] | select(.tag=="inbound-443") | .settings.clients[0].id')
+SID=$(echo "$CFG" | jq -r '.inbounds[] | select(.tag=="inbound-443") | .streamSettings.realitySettings.shortIds[0]')
+PRK=$(echo "$CFG" | jq -r '.inbounds[] | select(.tag=="inbound-443") | .streamSettings.realitySettings.privateKey')
+XPATH=$(echo "$CFG" | jq -r '.inbounds[] | select(.tag=="inbound-xhttp") | .streamSettings.xhttpSettings.path' | sed 's#^/##' )
+GNAME=$(echo "$CFG" | jq -r '.inbounds[] | select(.tag=="inbound-grpc") | .streamSettings.grpcSettings.serviceName')
 PBK=$(docker compose exec -T xray cat /home/PBK.txt)
+
+echo ""
 
 echo "Domain(SNI) = $DOM"
 echo "UUID = $UUID"
 echo "ShortID(SID) = $SID"
 echo "PrivateKey(PRK) = $PRK"
 echo "PublicKey(PBK) = $PBK"
-#echo "XHTTP_PATH = $XPATH"
+echo "XHTTP_PATH = $XPATH"
+echo "GRPC_NAME = $GNAME"
+
 echo ""
-echo "VLESS (TCP/Vision) = vless://${UUID}@${IP}:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${DOM}&fp=chrome&pbk=${PBK}&sid=${SID}&type=tcp&alpn=h2,http/1.1#${DOM}-Vision"
-echo "VLESS (XHTTP)      = vless://${UUID}@${IP}:8443?encryption=none&security=reality&sni=${DOM}&fp=chrome&pbk=${PBK}&sid=${SID}&type=xhttp&path=%2F&mode=auto&alpn=h2#${DOM}-XHTTP"
-#echo "VLESS (XHTTP)      = vless://${UUID}@${IP}:8443?encryption=none&security=reality&sni=${DOMAIN}&fp=chrome&pbk=${PBK}&sid=${SID}&type=xhttp&path=%2F${XPATH}&mode=auto&alpn=h2#${DOMAIN}-XHTTP"
+echo ""
+echo ""
+
+echo "VLESS (TCP/Vision) = vless://${UUID}@${IP}:443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${DOM}&fp=firefox&pbk=${PBK}&sid=${SID}&type=tcp&alpn=h2,http/1.1#${DOM}-Vision"
+
+echo ""
+echo ""
+
+echo "VLESS (XHTTP)      = vless://${UUID}@${IP}:8443?encryption=none&security=reality&sni=${DOM}&fp=firefox&pbk=${PBK}&sid=${SID}&type=xhttp&path=%2F${XPATH}&mode=auto&alpn=h2#${DOM}-XHTTP"
+
+echo ""
+echo ""
+
+echo "VLESS (gRPC)       = vless://${UUID}@${IP}:2053?encryption=none&security=reality&sni=${DOM}&fp=firefox&pbk=${PBK}&sid=${SID}&type=grpc&serviceName=${GNAME}&mode=gun#${DOM}-gRPC"
+
+echo ""
