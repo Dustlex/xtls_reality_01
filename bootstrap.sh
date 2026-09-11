937c96eec93f516e08604f090548c63f022df0b9
Dustlex <140817344+Dustlex@users.noreply.github.com>
2026-09-11 19:23:21 +0300

    Add GRPC_NAME asn XHTTP_PATH variable and output

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,11 @@ export PBK=${PBK:-$(echo "$K" | sed -n 2p)}
 export UUID=${UUID:-$(/opt/xray/xray uuid)}
 export SID=${SID:-$(openssl rand -hex 8)}
 export XHTTP_PATH=${XHTTP_PATH:-$(openssl rand -hex 8)}
+export GRPC_NAME=${GRPC_NAME:-$(openssl rand -hex 8)}
 
 echo "$PBK" > /home/PBK.txt
 echo "$XHTTP_PATH" > /home/XHTTP_PATH.txt
+echo "$GRPC_NAME" > /home/GRPC_NAME.txt
 
 envsubst < /opt/xray/config.json_prepare_2 > /opt/xray/config.json
 /opt/xray/xray run -c /opt/xray/config.json
